70776fea0d73028ed5c9c0e9bbd92d088493df64
양성호 <ysh8272002@naver.com>
2019-03-13 16:27:47 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o(#10m3p)v8bgwphmu*jry&o!)_cml7lik2)$u-wh7l$mty$t5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = '*'
 
